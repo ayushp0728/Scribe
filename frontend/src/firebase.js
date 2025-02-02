2597facbb9cb0e87adb2bf22b1e8a4c4adef87e4
Ayush Patel <ayush.patel1@rutgers.edu>
2025-02-01 20:13:38 -0500

    secrets oops

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -9,13 +9,7 @@ import { getFirestore, collection, addDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAU65TI5aX2ncXFFBW37LGc0LlIST04ZUc",
-  authDomain: "scribe-449619.firebaseapp.com",
-  projectId: "scribe-449619",
-  storageBucket: "scribe-449619.firebasestorage.app",
-  messagingSenderId: "524818858620",
-  appId: "1:524818858620:web:3099919058718538653ff9",
-  measurementId: "G-J7KLWKWDCX"
+  secret :3
 };
 
 
